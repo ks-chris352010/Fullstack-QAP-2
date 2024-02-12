f4fd41e0237506aa7e639ce0047ea927dab00951
Ks.chris352010 <136537957+ks-chris352010@users.noreply.github.com>
2024-02-12 17:56:25 -0330

    * Updated script to serve html files.

--- MultiRouteServer.js
+++ MultiRouteServer.js
@@ -1,46 +1,50 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 // Creates the server:
 const server = http.createServer((request, response) => {
+  const viewsPath = path.join(__dirname, "Views");
+  
+  // Displays the page to the user:
+  const connectPage = (filename) => {
+    const filePath = path.join(viewsPath, filename);
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        // If the page is not found displays an error in both the console and to the user.
+        console.error(`Error reading ${filename}: ${err.message}`);
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.end("Internal Server Error\n");
+      } else {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(data);
+        response.end();
+      }
+    })
+  }
   // Switch statement to connect the user to the page:
   switch (request.url) {
-    // Home page:
     case '/':
-      console.log('Home Page Requested');
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end('Home Page\n');
+      connectPage('home.html');
       break;
 
-    // About page:
     case '/about':
-      console.log('About Page Requested');
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end('About Page\n');
+      connectPage('about.html');
       break;
 
-    // Contact page:
     case '/contact':
-      console.log('Contact Page Requested');
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end('Contact Page\n');
+      connectPage('contact.html');
       break;
 
-    // Products page:
     case '/products':
-      console.log('Products Page Requested');
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end('Products Page\n');
-      break;
-    // Subscribe page:
-    case '/subscribe':
-      console.log('Subscribe Page Requested');
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end('Subscribe Page\n');
+      connectPage('products.html');
       break;
 
-    // Page not found:
+    case '/subscribe':
+      connectPage('subscribe.html');
+      break;
+
     default:
-      console.log('404 Not Found');
       response.writeHead(404, { 'Content-Type': 'text/plain' });
       response.end('404 Not Found\n');
   }
